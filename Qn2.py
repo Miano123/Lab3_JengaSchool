37d82852b40dd52eff67611b6ae74a483ce8502c
Miano123 <rachelmiano91@gmail.com>
2022-10-22 16:17:20 +0300

    String Variable

---
+++ Qn2.py
@@ -0,0 +1,16 @@
+# String variable to test out different results.
+
+def main():
+    
+    str = "This sentence has 27 chars."
+    print(len(str) # length of the string
+    print(str[-1]) #Prints out the first character of the string from right (.)
+    print(str[len](str)] #Error string index out of range
+    print(str[len(str)-1]) # Prints the first character of the string from the                              right(.)
+    print(str[4]) #Prints empty space in between the string
+    print(str[:4])# The first letter of the string
+    print(str[-6:])# Last word inclusive of character of the string
+    print(str[-9:-7])# Number of characters in the string
+    print(str[4:1])# Empty space in the string
+    print(str[-10:27])#prints out the number of characters and the last word.
+main()
