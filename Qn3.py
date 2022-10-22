a2c32b56ac9642d5b373241abc01382f6efc6ef0
Miano123 <rachelmiano91@gmail.com>
2022-10-22 18:04:14 +0300

    Printing individual characters of a string separated by open and close parentheses

---
+++ Qn3.py
@@ -0,0 +1,8 @@
+# Printing individual characters of a string seperated by open 
+# and close parentheses.
+
+def main():
+    str = "This sentence has 27 chars."
+    for c in str:
+        print(c.split("()"), end="")
+main()
