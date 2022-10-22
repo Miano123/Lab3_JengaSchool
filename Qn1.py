213587dab15b71841b8e2d19864207bb799d033f
Miano123 <rachelmiano91@gmail.com>
2022-10-22 15:52:28 +0300

    Index and slice

---
+++ Qn1.py
@@ -0,0 +1,13 @@
+# Using index function to find the first and last letters 
+#in a string variable.
+
+def main():
+    str = "Go home now"
+    print(str([0]+str[-1])
+
+# Slice function to print the first and last words of a string without space.
+    x = slice(0,2)
+    #x=slice(-3,-1)
+    y = slice(8,11)
+    print(str[x]+str[y])
+main()
